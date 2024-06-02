c30e1e7b332110da41b849f1b4dc749b99222875
Wadha Bin Awn <wadhabinawn@gmail.com>
2024-06-02 12:10:19 +0300

    add quiz

---
+++ src/main/java/Day4/JobDAO.java
@@ -0,0 +1,67 @@
+package Day4;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class JobDAO {
+
+    private static final String URL = "jdbc:sqlite:D:\\Private\\SDAIA\\SDAIA Java Course\\JavaBasics\\src\\main\\java\\day4\\hr.db";
+    private static final String SELECT_ALL_JOBS = "select * from jobs";
+    private static final String SELECT_ONE_JOB = "select * from jobs where job_id = ?";
+    private static final String INSERT_JOB = "insert into jobs values (?, ?, ?, ?)";
+    private static final String UPDATE_JOB = "update jobs set job_title = ?, min_salary = ?, max_salary = ? where job_id = ?";
+    private static final String DELETE_JOB = "delete from jobs where job_id = ?";
+
+    public void insertJob(Jobs j) throws SQLException {
+        Connection conn = DriverManager.getConnection(URL);
+        PreparedStatement st = conn.prepareStatement(INSERT_JOB);
+        st.setInt(1, j.getJob_id());
+        st.setString(2, j.getJob_title());
+        st.setDouble(3, j.getMin_salary());
+        st.setDouble(4, j.getMax_salary());
+        st.executeUpdate();
+    }
+
+    public void updateJob(Jobs j) throws SQLException {
+        Connection conn = DriverManager.getConnection(URL);
+        PreparedStatement st = conn.prepareStatement(UPDATE_JOB);
+        st.setInt(4, j.getJob_id());
+        st.setString(1, j.getJob_title());
+        st.setDouble(2, j.getMin_salary());
+        st.setDouble(3, j.getMax_salary());
+        st.executeUpdate();
+    }
+
+    public void deleteJob(String jobId) throws SQLException {
+        Connection conn = DriverManager.getConnection(URL);
+        PreparedStatement st = conn.prepareStatement(DELETE_JOB);
+        st.setString(1, jobId);
+        st.executeUpdate();
+    }
+
+    public Jobs selectJob(String jobId) throws SQLException {
+        Connection conn = DriverManager.getConnection(URL);
+        PreparedStatement st = conn.prepareStatement(SELECT_ONE_JOB);
+        st.setString(1, jobId);
+        ResultSet rs = st.executeQuery();
+        if(rs.next()) {
+            return new Jobs(rs);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public ArrayList<Jobs> selectAllJobs() throws SQLException {
+        Connection conn = DriverManager.getConnection(URL);
+        PreparedStatement st = conn.prepareStatement(SELECT_ALL_JOBS);
+        ResultSet rs = st.executeQuery();
+        ArrayList<Jobs> jobs = new ArrayList<>();
+        while (rs.next()) {
+            jobs.add(new Jobs(rs));
+        }
+
+        return jobs;
+    }
+
+}
