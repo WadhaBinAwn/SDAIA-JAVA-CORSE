91bfdc5b893bbf9e48fd7bbf107199bb33f31c0c
Wadha Bin Awn <wadhabinawn@gmail.com>
2024-06-02 08:38:54 +0300

    add hw

---
+++ src/main/java/HomeWork/Students.java
@@ -0,0 +1,25 @@
+package HomeWork;
+
+public class Students {
+
+    private String name;
+    private int id;
+
+    public Students(String name, int id){
+        this.name = name;
+        this.id = id;
+
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+
+    public int getId(){
+        return id;
+    }
+
+
+}
